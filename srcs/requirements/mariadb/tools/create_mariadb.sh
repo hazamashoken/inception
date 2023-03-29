593187645c46a4b906640edf58cc074657e97efc
tliangso <earth78203@gmail.com>
2023-03-29 13:39:23 +0800

    mariadb -> mysql

--- srcs/requirements/mariadb/tools/create_mariadb.sh
+++ srcs/requirements/mariadb/tools/create_mariadb.sh
@@ -11,34 +11,32 @@ mysqld -u mysql --skip-networking --initialize-insecure &
 service mysql start
 
 echo Remove test Database
-mariadb -u root -e "DROP DATABASE IF EXISTS test;"
+mysql -u root -e "DROP DATABASE IF EXISTS test;"
 
 echo Remove anonymous users
-mariadb -u root -e "DELETE FROM mysql.user WHERE User='';"
+mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
 
-#echo Remove remote root access
-#mariadb -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-
+echo Remove remote root access
+mariadb -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 
 echo Creating database
 echo "CREATE DATABASE ${MYSQL_NAME}"
-mariadb -u root -e "CREATE DATABASE ${MYSQL_NAME}"
+mysql -u root -e "CREATE DATABASE ${MYSQL_NAME}"
 
 echo Creating user
 echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mariadb -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
 echo Grant user privileges
 echo "GRANT ALL PRIVILEGES ON '${MYSQL_NAME}'.* TO '${MYSQL_USER}'@'%';"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON '${MYSQL_NAME}'.* TO '${MYSQL_USER}'@'%';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON '${MYSQL_NAME}'.* TO '${MYSQL_USER}'@'%';"
 
 echo Flush privileges
-mariadb -u root -e "FLUSH PRIVILEGES;"
+mysql -u root -e "FLUSH PRIVILEGES;"
 
 echo Change root password
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
 echo stoping mariadb server
 service mysql stop
