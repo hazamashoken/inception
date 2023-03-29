0c036ea390660396c8d5472a9db5c47cf0391718
tliangso <earth78203@gmail.com>
2023-03-29 13:50:42 +0800

    TEST

--- srcs/requirements/mariadb/tools/create_mariadb.sh
+++ srcs/requirements/mariadb/tools/create_mariadb.sh
@@ -6,8 +6,8 @@ mkdir -p /var/lib/mysql /var/run/mysqld
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
-echo Running maridb server in the background ...
-mysqld -u mysql --skip-networking --initialize-insecure &
+echo Running mariadb server in the background ...
+mysqld -u mysql --skip-networking --initialize-insecure
 service mysql start
 
 echo Waiting for mariadb server to start ...
@@ -18,33 +18,33 @@ do
 	mariadb -u root -e "SELECT version();" > /dev/null 2>&1
 done
 
-echo Remove test Database
-mariadb -u root -e "DROP DATABASE IF EXISTS test;"
+# echo Remove test Database
+# mariadb -u root -e "DROP DATABASE IF EXISTS test;"
 
-echo Remove anonymous users
-mariadb -u root -e "DELETE FROM mysql.user WHERE User='';"
+# echo Remove anonymous users
+# mariadb -u root -e "DELETE FROM mysql.user WHERE User='';"
 
-echo Remove remote root access
-mariadb -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+# echo Remove remote root access
+# mariadb -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 
-echo Creating database
-echo "CREATE DATABASE ${MYSQL_NAME}"
-mariadb -u root -e "CREATE DATABASE ${MYSQL_NAME}"
+# echo Creating database
+# echo "CREATE DATABASE ${MYSQL_NAME}"
+# mariadb -u root -e "CREATE DATABASE ${MYSQL_NAME}"
 
-echo Creating user
-echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mariadb -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# echo Creating user
+# echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+# mariadb -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-echo Grant user privileges
-echo "GRANT ALL PRIVILEGES ON '${MYSQL_NAME}'.* TO '${MYSQL_USER}'@'%';"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON '${MYSQL_NAME}'.* TO '${MYSQL_USER}'@'%';"
+# echo Grant user privileges
+# echo "GRANT ALL PRIVILEGES ON '${MYSQL_NAME}'.* TO '${MYSQL_USER}'@'%';"
+# mariadb -u root -e "GRANT ALL PRIVILEGES ON '${MYSQL_NAME}'.* TO '${MYSQL_USER}'@'%';"
 
-echo Flush privileges
-mariadb -u root -e "FLUSH PRIVILEGES;"
+# echo Flush privileges
+# mariadb -u root -e "FLUSH PRIVILEGES;"
 
-echo Change root password
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+# echo Change root password
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+# mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
 echo stoping mariadb server
 service mysql stop
