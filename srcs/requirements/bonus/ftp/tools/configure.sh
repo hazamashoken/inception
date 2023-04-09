b32a5c4569450f5fa86a87a164c30085d65d1aff
Your Name <you@example.com>
2023-04-09 14:17:22 +0700

    final

--- srcs/requirements/bonus/ftp/tools/configure.sh
+++ srcs/requirements/bonus/ftp/tools/configure.sh
@@ -6,13 +6,16 @@ if [ ! -f "/etc/vsftpd/vsftpd.conf.bak" ]; then
     mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
 
     # Add the FTP_USER, change his password and declare him as the owner of wordpress folder and all subfolders
+
+	echo Adding FTP user $FTP_USER
     adduser $FTP_USER --disabled-password
     echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd &> /dev/null
+	echo "$FTP_USER:$FTP_PASS"
     chown -R $FTP_USER:$FTP_USER /var/www/html
 
     echo $FTP_USER | tee -a /etc/vsftpd.userlist &> /dev/null
 
 fi
 
-echo "FTP started on 20:21:40000-40009"
-/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+echo "FTP started on 20:21:13450-13500"
+
